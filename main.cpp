44a3157dfca6969cab6c9af40fab5f1b0fb76e99
Umar <umarzaidtenby@gmail.com>
2026-06-24 10:27:22 +0800

    Added memory

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <cstdint>
 
 using namespace std;
 
@@ -8,9 +10,23 @@ using namespace std;
 class Register
 {
     protected:
-        int value; // value of the 8-bit itself
+        int8_t value; // value of the 8-bit itself
 
     public:
+        Register() : value()
+        {
+
+        }
+
+        void setValue(int8_t val)
+        {
+            value = val;
+        }
+
+        int8_t getValue() const
+        {
+            return value;
+        }
 
 };
 
@@ -35,9 +51,15 @@ class Instruction
 
 
 // Handles 'ADD', 'SUB' and other arithmetic instructions for assembly
-class ArithmeticInstruction : Instruction
-{   
-    // insert code here
+class ArithmeticInstruction : public Instruction
+{
+    public:
+        void execute() override
+        {
+            // implementation goes here!
+        }
+    
+    
 };
 
 // handles inputOutput command for assembly instructions
@@ -63,6 +85,37 @@ class FlagRegister
 // Handles storage and addressing logic over a vector of bytes
 class Memory
 {
+    private:
+        vector<int8_t> storage;
+
+    public:
+        Memory(size_t size = 64) : storage(size,0)
+        {
+
+        }
+
+        void write(size_t address, int8_t value)
+        {
+            if (address < storage.size())
+            {
+                storage[address] = value;
+            }
+            else
+            {
+                cerr << "Memory Access Violation: Write at " << address << endl;
+            }
+        }
+
+        int8_t read(size_t address) const
+        {
+            if (address < storage.size())
+            {
+                return storage[address];
+            }
+
+            cerr << "Memory Access Violation: Read at " << address << endl;
+            return 0;
+        }
     //insert code here
 };
 
@@ -72,7 +125,8 @@ class CPU
     private:
         Memory memory;          // composition: CPU owns memory
         FlagRegister* flags;    // aggregation: CPU owns external flag register
-
+        GeneralRegister registers[8];
+        int pc;
 };
 
 // Loads programs, decodes instructions, delegates execution to `CPU`
